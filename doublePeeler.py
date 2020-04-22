44269c47b8bde5f529577f1f3d8a62f3f7760099
Aayush Tiwari <aayusht4736@gmail.com>
2020-04-22 14:47:49 +0530

    LDPC Enocder added
    LDPC parity check matrix is tested for N=12500, k=10000, d=8.
    Runs fine.

--- doublePeeler.py
+++ doublePeeler.py
@@ -1,38 +1,86 @@
 import math
-import numpy as np 
+# import numpy as np 
 import random
-from scipy import *
+# from scipy import *
 from random import choices
 
-def LDPCMatrix():
+#output to be stored at nodes	
+codedBlocks=[]
 
-
+#PC matrix is of dimension (N-k)x(N)
+#we will store positions of 1's
+#N-k = No. of parity eqautions
+#k = input symbols 
+#d = check nodes degree which is constant
+def LDPCMatrix(N,k,d):
+	SumC=[0]*(k)
+	SumR=[0]*(N-k)
+	count1=0
+	count2=0
+	PCMatrix=[]
+	index=[i for i in range(0,k)]
+	for i in range (0,N-k):
+		# print(i, end=" ")
+		for j in range (0, d-1):
+			flag=1
+			while(flag==1):
+				a=random.choice(index)
+				flag=0
+				if(SumC[a]==2):
+					index.remove(a)
+					flag=1	
+				# if(SumC[a]==1):
+				# 	count1+=1
+				# 	if(count1>((N-k)/5)*2):
+				# 		# print("yes")
+				# 		flag=1
+				# # elif(SumC[a]<2):
+				# 	count2+=1
+				# 	if(count2>((N-k)/5)*3):
+				# 		flag=1
+			PCMatrix+=[i,a]
+			SumC[a]+=1
+			SumR[i]+=1
+		
+	for i in range(0,len(SumR)):
+		print(SumR[i], end=" ")
+	
+LDPCMatrix(12500, 10000, 8)
 
 def LTCodeDistribution(N):
 	epsilon = 2.0/3.0 #for k=10,000 input symbols
 	mhu= (2.0*epsilon + epsilon*epsilon)/4.0 # mhu = eps/2 + (eps/2)^2
-	D=10 #max-degree=upperBound((4*(1+eps))/eps) 
+	D=9.0 #max-degree=upperBound((4*(1+eps))/eps) 
 
 	#defining coefficients of x^i
-	probabilities = [0, mhu/(mhu+1)]
-	probabilities += [1.0/(mhu+1)*((i)*(i-1)) for i in range (2,D)]
-	probabilities+=[1.0/(mhu+1)*D]
-	probabilities += [0 for i in range (D,N-1)]
-
+	probabilities = [0.0, mhu/(mhu+1)]
+	probabilities += [1.0/((mhu+1)*((i)*(i-1))) for i in range (2,int(D)+1)]
+	probabilities+=[1.0/((mhu+1)*D)]
+	probabilities += [0.0 for i in range (int(D)+2,int(N))]
+	su=0.0
+	for i in range(0,len(probabilities)):
+		if(i<=12):
+			# probabilities[i]='{:<020}'.format(probabilities[i])
+			# print(probabilities[i])
+			su+=(probabilities[i])
+		else:
+			break
+	# print ("prob",probabilities)
+	print(su)
 	return probabilities
 
 #returns the value d for a block i.e degree for any block
 def getDegrees(N,c):
 	probabilities= LTCodeDistribution(N)
 
-	blocks = [i for i in range(N)]
+	blocks = [i for i in range(0,int(N))]
 	#c=droplet quantity
 	#N=k+p
-	return choices(blocks, weights=probabilities, k=c)
+	return choices(blocks, weights=probabilities, k=int(c))
 
 
 class codedBlockStruct: 
-	def __init__(self, degree, neighbours, flag,data): #header might be needed in future
+	def __init__(self, degree, neighbours, flag): #header might be needed in future
 		self.degree=degree
 		self.neighbours=neighbours
 		self.flag=0 #checks maliciousness of a block
@@ -40,21 +88,19 @@ class codedBlockStruct:
 
 #returns indexes for a given block provided degree=d
 def getIndex(deg,blocksN):
-	random.seed(deg)
+	# random.seed(deg)
 	indexes= random.choices(range(blocksN), k=deg)
 	return indexes
 
 def encoder():
 	N=12500.0
-	c=25.0 #for starge saving = 500
+	c=10.0 #for starge saving = 500
 	deg = getDegrees(N,c)
-	print "deg",deg 
-	codedBlocks=[]
-	for i in range(c):
-		indexes=getIndex(deg)
-		print "index",indexes
-		codedBlock.degree=deg
-		codedBlock.neighbours=indexes
+	print ("deg",deg, "length", len(deg)) 
+	for i in range(int(c)):
+		indexes=getIndex(deg[i],12500)
+		print ("index",indexes)
+		codedBlock=codedBlockStruct(deg[i],indexes,0)
 		# codedBlock.data=blocks[indexes[0]]
 		# for j in range(1,deg):
 		# 	codedBlock.data= codedBlock.data^blocks[j]
@@ -62,7 +108,7 @@ def encoder():
 		# codedBlocks += codedBlock
 
 	return codedBlocks
-encoder()
+# encoder()
 
 def removeDegree(block, codedBlocks):
 	for codedBlock in codedBlocks:
@@ -87,3 +133,4 @@ def decoder(codedBlocks,N,D):
 				else:
 					#to make it marks as a malicious nodes
 					codedBlock.flag=1
+# decoder(codedBlocks,12500,9)
