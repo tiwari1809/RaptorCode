8df0173607718a49ace5920d3b93a33af612f821
Aayush Tiwari <aayusht4736@gmail.com>
2020-05-16 16:15:35 +0530

    Added prob for 0 degree

--- LTCodeDegreeDistribution.py
+++ LTCodeDegreeDistribution.py
@@ -6,11 +6,11 @@ def LTCodeDegreeDistribution(N):
 	D=10 #max-degree=upperBound((4*(1+eps))/eps) 
 
 	#defining coefficients of x^i
-	#used round function to roundoff the last digit for non-terminating decimal
-	probabilities= [round(mhu/(mhu+1),15)]
+	#used round function to roundoff the last(15th) digit for non-terminating decimal
+	probabilities= [0,round(mhu/(mhu+1),15)]
 	probabilities+=[round(1/((mhu+1)*((i)*(i-1))), 15) for i in range (2,D+1)]
 	probabilities+=[round(1/((mhu+1)*D), 15)]
-	probabilities+=[0 for i in range (D+2,N+1)]
+	probabilities+=[0 for i in range (D+2,N)]
 	su=0
 	for i in range(0,len(probabilities)):
 		if(i<=12):
