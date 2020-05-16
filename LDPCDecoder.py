9e99883c43d5afab494f17bf7e12a2384a2d0e7b
Aayush Tiwari <aayusht4736@gmail.com>
2020-05-16 16:17:38 +0530

    Changed decoding fucntion

--- LDPCDecoder.py
+++ LDPCDecoder.py
@@ -1,31 +1,31 @@
 from source import *
 
 def LDPCDecoder(IntermediateBlocks, recoveredBlocks, NoOfNodesDownloaded, k):
-	count=0
-	NotRecovered=None
-	flag=0
 	originalBlocks=set()
+	NotRecovered=None
 	
 	for block in recoveredBlocks:
-		if(int(block) <= (k)):
+		if(int(block) < (k)):
 			originalBlocks.add(block)
-	print("pre LDPC orig block len: ",len(originalBlocks))
-	while(len(originalBlocks)<(k)):
+	print("pre LDPC decoding, No. of i/p blocks recovered: ",len(originalBlocks))
+	
+	while(len(originalBlocks)<k):
 		flag=0
+		for blocks in IntermediateBlocks:
+			flag=0
 		for blocks in IntermediateBlocks:
 			count=0
 			for block in blocks:
 				if(block not in recoveredBlocks):
 					count+=1
 					NotRecovered=block
+
 			if(count==1):
 				flag=1
 				recoveredBlocks.add(NotRecovered)
-				if(int(NotRecovered) <= (k)):
+				if(int(NotRecovered)<(k)):
 					originalBlocks.add(NotRecovered)
 				
 		if(flag==0):
-			# print("Recalling")
-			# decoder(3000,NoOfNodesDownloaded+25)
 			break
 	return originalBlocks
